9fafe2b86f753ab579457a7b56e18b9b73a9bb9d
John Bower <john@industrialassets.co.uk>
2024-10-14 14:10:25 +0100

    import options data

--- src/api/router.js
+++ src/api/router.js
@@ -1,5 +1,6 @@
 import { health } from '~/src/api/health/index.js'
 import { example } from '~/src/api/example/index.js'
+import { importData } from '~/src/api/importData/index.js'
 
 /**
  * @satisfies { import('@hapi/hapi').ServerRegisterPluginObject<*> }
@@ -10,6 +11,9 @@ const router = {
     register: async (server) => {
       // Health-check route. Used by platform to check if service is running, do not remove!
       await server.register([health])
+
+      // Import data endpoints
+      await server.register([importData])
 
       // Application specific routes, add your own routes here.
       await server.register([example])
