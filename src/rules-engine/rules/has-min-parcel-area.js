9e9da405b09a7986971257db611ef025b9a5b1e5
Nick Froud <nfroud@gmail.com>
2024-10-15 11:59:05 +0100

    added rules engine

---
+++ src/rules-engine/rules/has-min-parcel-area.js
@@ -0,0 +1,16 @@
+const hasMinimumParcelArea = (application, ruleConfig) => {
+  const {
+    landParcel: { area }
+  } = application
+
+  const passed = area >= ruleConfig.minArea
+
+  return !passed
+    ? {
+        passed,
+        message: `The parcel must have a total area of at least ${ruleConfig.minArea}ha`
+      }
+    : { passed }
+}
+
+module.exports = { hasMinimumParcelArea }
---
+++ src/rules-engine/rulesEngine.js
@@ -0,0 +1,28 @@
+const { rules: rulesLibrary } = require('./rules')
+
+const executeRule = (ruleName, application, ruleConfig) => {
+  const rule = rulesLibrary[ruleName]
+
+  if (!rule) {
+    throw new Error(`Unknown rule: ${ruleName}`)
+  }
+
+  return rule(application, ruleConfig)
+}
+
+const executeRules = (application, rules) => {
+  if (!rules?.length) {
+    throw new Error('No rules provided to execute')
+  }
+  const results = rules.map((rule) => ({
+    ruleName: rule.id,
+    ...executeRule(rule.id, application, rule.config)
+  }))
+
+  return { results, passed: results.every((result) => result.passed === true) }
+}
+
+module.exports = {
+  executeRule,
+  executeRules
+}
