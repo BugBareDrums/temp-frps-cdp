53aeb079bf9b2ada3bfe5e572f1403edf5f3295e
John Bower <john@industrialassets.co.uk>
2024-10-23 17:07:22 +0100

    remove land cover from seed

--- src/helpers/seed-db/data/index.js
+++ src/helpers/seed-db/data/index.js
@@ -1,9 +1,7 @@
 import { farmers } from './farmers.js'
 import { actions } from './actions.js'
-import { landCover } from './land-cover/index.js'
 
 export default {
   farmers,
-  actions,
-  landCover
+  actions
 }
