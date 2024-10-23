cc3ce7086f43c4cf91c556955c04d65061e8e49d
John Bower <john@industrialassets.co.uk>
2024-10-23 17:06:51 +0100

    created import land codes endpoint

--- src/helpers/seed-db/data/index.js
+++ src/helpers/seed-db/data/index.js
@@ -1,7 +1,9 @@
 import { farmers } from './farmers.js'
 import { actions } from './actions.js'
+import { landCover } from './land-cover/index.js'
 
 export default {
   farmers,
-  actions
+  actions,
+  landCover
 }
