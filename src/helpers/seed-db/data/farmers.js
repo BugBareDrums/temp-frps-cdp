e7052330999ba678b74154990b0265338ec7f187
John Bower <john@industrialassets.co.uk>
2024-10-30 11:06:05 +0000

    added more seed data

--- src/helpers/seed-db/data/farmers.js
+++ src/helpers/seed-db/data/farmers.js
@@ -1,8 +1,88 @@
-// eslint-disable-next-line n/no-unpublished-import
-import { faker } from '@faker-js/faker'
-
-const farmers = Array.from({ length: 5000 }, () => ({
-  name: `${faker.person.firstName()} ${faker.person.lastName()}`
-}))
-
-export { farmers }
+export const farmers = [
+  {
+    crn: '1789678903',
+    firstName: 'Farmer',
+    lastName: 'Sarah',
+    email: 'Sarah@testnull.com',
+    landlinePhone: '056712678',
+    mobilePhone: '012345678',
+    businesSize: 'Micro',
+    numberOfEmployees: 5,
+    areasManaged: {
+      agricultural: '20 Hectares',
+      horticultural: '5 Hectares',
+      woodland: '5 Hectares'
+    },
+    registeredOwner: 'Farmer Sarah',
+    gender: 'Female',
+    address: 'Imaginary Farm Co, Suffolk, SF1 23ED',
+    businessType: 'Farm',
+    activity: 'Arable Farm',
+    companies: [
+      {
+        name: 'Imaginary Farm',
+        id: 'imaginary-farm',
+        sbi: '908789876'
+      },
+      {
+        name: 'Apple Farm',
+        id: 'apple-farm',
+        sbi: '678987809'
+      }
+    ]
+  },
+  {
+    crn: '3567890743',
+    firstName: 'Farmer',
+    lastName: 'Gill',
+    email: 'Gill@testnull.com',
+    landlinePhone: '076575678',
+    mobilePhone: '076575678',
+    businesSize: 'Micro',
+    numberOfEmployees: 7,
+    areasManaged: {
+      agricultural: '25 Hectares',
+      horticultural: '10 Hectares',
+      woodland: '5 Hectares'
+    },
+    registeredOwner: 'Farmer Gill',
+    gender: 'Female',
+    address: 'Berry Farm, Norfolk, NO4 3RF',
+    businessType: 'Farm',
+    activity: 'Arable Farm',
+    companies: [
+      {
+        name: 'Berry Farm',
+        id: 'berry-farm',
+        sbi: '786756789'
+      }
+    ]
+  },
+  {
+    crn: '7892345678',
+    firstName: 'Farmer',
+    lastName: 'Tom',
+    email: 'Tom@testnull.com',
+    landlinePhone: '086127891',
+    mobilePhone: '034567891',
+    businesSize: 'Micro',
+    numberOfEmployees: 6,
+    areasManaged: {
+      agricultural: '15 Hectares',
+      horticultural: '5 Hectares',
+      woodland: '10 Hectares'
+    },
+    registeredOwner: 'Farmer Tom',
+    gender: 'Male',
+    address: 'Green Farm, Devon, DV6 4RH',
+    businessType: 'Farm',
+    activity: 'Arable Farm',
+    companies: [
+      {
+        name: 'Green Farm',
+        id: 'green-farm',
+        sbi: '767892234'
+      }
+    ]
+  }
+]
--- src/helpers/seed-db/data/index.js
+++ src/helpers/seed-db/data/index.js
@@ -1,7 +1,9 @@
 import { farmers } from './farmers.js'
 import { actions } from './actions.js'
+import { parcels } from './parcels.js'
 
 export default {
   farmers,
-  actions
+  actions,
+  parcels
 }
