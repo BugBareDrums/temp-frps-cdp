86938cd8ac5f3de5aa43160a7b6e4b242ac9d192
Bill Sclater <bill.sclater@equalexperts.com>
2024-11-05 08:35:08 +0000

    added csam3 action and its land use compatibility

--- src/api/available-area/helpers/action-land-use-compatibility-matrix.js
+++ src/api/available-area/helpers/action-land-use-compatibility-matrix.js
@@ -8,6 +8,9 @@ function applyUpdate(newEntries, actionCombinationLandUseCompatibilityMatrix) {
 //  create an algorithm to generate matrix of possible combinations instead of hardcoding combinations
 const ACTION_COMBO_LAND_USE_COMPATIBILITY_MATRIX = {
   AC32: [
+    ['CSAM3'],
+    ['CSAM3', 'SAM1'],
+    ['CSAM3', 'CSAM1'],
     ['SAM1', 'SAM2'],
     ['SAM1'],
     ['SAM2'],
@@ -25,6 +28,9 @@ const ACTION_COMBO_LAND_USE_COMPATIBILITY_MATRIX = {
     ['CSAM2', 'CSAM1', 'AB3']
   ],
   PG01: [
+    ['CSAM3'],
+    ['CSAM3', 'SAM1'],
+    ['CSAM3', 'CSAM1'],
     ['SAM1', 'SAM3'],
     ['SAM1', 'LIG1'],
     ['SAM1', 'GRH1'],
@@ -36,6 +42,44 @@ const ACTION_COMBO_LAND_USE_COMPATIBILITY_MATRIX = {
     ['CSAM1', 'LIG1'],
     ['CSAM1', 'GRH1'],
     ['CSAM1']
+  ],
+  TG01: [
+    ['AB3'],
+    ['AB3', 'CSAM1'],
+    ['AB3', 'SAM1'],
+    ['CSAM1'],
+    ['CSAM1', 'AB3'],
+    ['CSAM1', 'CSAM2'],
+    ['CSAM1', 'CSAM3'],
+    ['CSAM1', 'GRH1'],
+    ['CSAM1', 'LIG1'],
+    ['CSAM1', 'SAM2'],
+    ['CSAM1', 'SAM3'],
+    ['CSAM2'],
+    ['CSAM2', 'SAM1'],
+    ['CSAM3'],
+    ['CSAM3', 'CSAM1'],
+    ['CSAM3', 'SAM1'],
+    ['GRH1'],
+    ['GRH1', 'CSAM1'],
+    ['GRH1', 'SAM1'],
+    ['LIG1'],
+    ['LIG1', 'CSAM1'],
+    ['LIG1', 'SAM1'],
+    ['SAM1'],
+    ['SAM1', 'AB3'],
+    ['SAM1', 'CSAM2'],
+    ['SAM1', 'CSAM3'],
+    ['SAM1', 'GRH1'],
+    ['SAM1', 'LIG1'],
+    ['SAM1', 'SAM2'],
+    ['SAM1', 'SAM3'],
+    ['SAM2'],
+    ['SAM2', 'CSAM1'],
+    ['SAM2', 'SAM1'],
+    ['SAM3'],
+    ['SAM3', 'CSAM1'],
+    ['SAM3', 'SAM1']
   ]
 }
 
