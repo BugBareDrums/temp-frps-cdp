a496098e5d950b76d430379b06e97810a88828b8
John Bower <john@industrialassets.co.uk>
2024-11-06 17:19:15 +0000

    working action validation controller

--- src/api/action/controllers/action-validation-controller.js
+++ src/api/action/controllers/action-validation-controller.js
@@ -1,30 +1,161 @@
 import Boom from '@hapi/boom'
+import Joi from 'joi'
 import { findAction } from '../helpers/find-action.js'
+import { actionCombinationLandUseCompatibilityMatrix } from '~/src/api/available-area/helpers/action-land-use-compatibility-matrix.js'
+import { executeRules } from '~/src/rules-engine/rulesEngine.js'
+
+const isValidCombination = (
+  preexistingActions = [],
+  userSelectedActions,
+  landUseCodes
+) => {
+  const actionCodes = userSelectedActions
+    .concat(preexistingActions)
+    .map((action) => action.actionCode)
+  for (const code of landUseCodes) {
+    const allowedCombinations =
+      actionCombinationLandUseCompatibilityMatrix[code] || []
+    let validForThisCode = false
+    for (const combination of allowedCombinations) {
+      if (actionCodes.every((actionCode) => combination.includes(actionCode))) {
+        validForThisCode = true
+        break
+      }
+    }
+    if (!validForThisCode) {
+      if (preexistingActions.length > 0) {
+        const actionCodesString = preexistingActions
+          .map((action) => action.actionCode)
+          .join(', ')
+        return [
+          `The selected combination of actions, along with your pre-existing actions (${actionCodesString}), is invalid for land use code ${code}`
+        ]
+      }
+      return [
+        `The selected combination of actions are invalid for land use code: ${code}`
+      ]
+    }
+  }
+}
+
+/**
+ *
+ * @param { import('mongodb').Db } db
+ * @param { Array } userSelectedActions
+ * @param { object } landParcel
+ * @returns
+ */
+const executeActionRules = async (db, userSelectedActions, landParcel) => {
+  const actionPromises = await Promise.all(
+    userSelectedActions.map(
+      async (action) => await findAction(db, action.actionCode)
+    )
+  )
+
+  return userSelectedActions.map((action, index) => {
+    const application = {
+      areaAppliedFor: parseFloat(action.quantity),
+      actionCodeAppliedFor: action.actionCode,
+      landParcel: {
+        area: parseFloat(landParcel.area),
+        moorlandLineStatus: landParcel.moorlandLineStatus,
+        existingAgreements: []
+      }
+    }
+    const userSelectedAction = actionPromises[index]
+
+    return {
+      action: action.actionCode,
+      ...executeRules(application, userSelectedAction.eligibilityRules)
+    }
+  })
+}
 
 /**
  *
  * @satisfies {Partial<ServerRoute>}
  */
 const actionValidationController = {
+  options: {
+    validate: {
+      payload: Joi.object({
+        actions: Joi.array().required(),
+        landParcel: Joi.object().required()
+      }).required(),
+      failAction: (request, h, error) => {
+        return Boom.badRequest(error)
+      }
+    }
+  },
+
   /**
-   * @param { import('@hapi/hapi').Request & MongoDBPlugin } request
-   * @returns {Promise<*>}
+   * @typedef { import('@hapi/hapi').Request & object } RequestPayload
+   * @property { object } landParcel
+   * @property { Array } actions
    */
-  handler: async ({ db, params: { actionCode } }, h) => {
-    if (!actionCode || actionCode === '')
-      return Boom.badRequest('Missing actionCode query parameter')
 
-    const action = await findAction(db, actionCode)
+  /**
+   * @param { import('@hapi/hapi').Request & MongoDBPlugin & RequestPayload } request
+   * @returns { Promise<*> }
+   */
+  handler: async ({ db, payload: { actions, landParcel } }, h) => {
+    const errors = {
+      ...isValidCombination(
+        landParcel.agreements,
+        actions,
+        landParcel.landUseCodes
+      )
+    }
 
-    if (!action) return Boom.notFound(`Action ${actionCode} not found`)
+    if (errors.length) {
+      return h
+        .response(
+          JSON.stringify({
+            message: errors,
+            isValidCombination: false
+          })
+        )
+        .code(200)
+    }
 
-    return h.response({ action }).code(200)
+    const ruleResults = await executeActionRules(db, actions, landParcel)
+    const ruleFailureMessages = []
+    for (const result of ruleResults) {
+      if (!result.passed) {
+        const results = result.results
+          .filter((r) => !r.passed)
+          .map((r) => r.message)
+          .join(', ')
+
+        ruleFailureMessages.push(`${result.action}: ${results}`)
+      }
+    }
+
+    if (ruleFailureMessages.length) {
+      return h
+        .response(
+          JSON.stringify({
+            message: ruleFailureMessages.join(', '),
+            isValidCombination: false
+          })
+        )
+        .code(200)
+    }
+
+    return h
+      .response(
+        JSON.stringify({
+          message: 'Action combination valid',
+          isValidCombination: true
+        })
+      )
+      .code(200)
   }
 }
 
 export { actionValidationController }
 
 /**
- * @import { ServerRoute} from '@hapi/hapi'
+ * @import { ServerRoute } from '@hapi/hapi'
  * @import { MongoDBPlugin } from '~/src/helpers/mongodb.js'
  */
--- src/api/action/index.js
+++ src/api/action/index.js
@@ -1,6 +1,7 @@
 import { getActionController } from './controllers/get-action-controller.js'
 import { getAllActionsController } from './controllers/get-all-actions-controller.js'
 import { postActionRuleController } from './controllers/post-action-rule-controller.js'
+import { actionValidationController } from './controllers/action-validation-controller.js'
 
 /**
  * @satisfies {ServerRegisterPluginObject<void>}
@@ -10,6 +11,11 @@ const action = {
     name: 'action',
     register: (server) => {
       server.route([
+        {
+          method: 'POST',
+          path: '/action-validation',
+          ...actionValidationController
+        },
         {
           method: 'GET',
           path: '/action/{actionCode}',
